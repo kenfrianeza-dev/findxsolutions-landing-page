b9452658fc3ead059b133e87bc6ffc7172cbdce8
Keynn2555 <75205651+Keynn2555@users.noreply.github.com>
2023-07-23 06:29:23 +0800

    products name changed to plural

--- src/app/constants/products/data.ts
+++ src/app/constants/products/data.ts
@@ -1,169 +1,169 @@
 export const items = [
   {
     category: 'Office Equipments',
-    name: 'Camera',
+    name: 'Cameras',
     image_path: '/assets/product-thumbnail/office-equipments/camera.jpg',
     slug: 'office-equipments'
   },
   {
     category: 'Office Equipments',
-    name: 'Laptop',
+    name: 'Laptops',
     image_path: '/assets/product-thumbnail/office-equipments/laptop.jpg',
     slug: 'office-equipments'
   },
   {
     category: 'Office Equipments',
-    name: 'Mouse',
+    name: 'Mice',
     image_path: '/assets/product-thumbnail/office-equipments/mouse.jpg',
     slug: 'office-equipments'
   },
   {
     category: 'Office Equipments',
-    name: 'Printer',
+    name: 'Printers',
     image_path: '/assets/product-thumbnail/office-equipments/printer.jpg',
     slug: 'office-equipments'
   },
   {
     category: 'Advertising Goods',
-    name: 'Signage',
+    name: 'Signages',
     image_path: '/assets/product-thumbnail/advertising-goods/signage.jpg',
     slug: 'advertising-goods'
   },
   {
     category: 'Advertising Goods',
-    name: 'Poster',
+    name: 'Posters',
     image_path: '/assets/product-thumbnail/advertising-goods/poster.jpg',
     slug: 'advertising-goods'
   },
   {
     category: 'Advertising Goods',
-    name: 'Tarpaulin',
+    name: 'Tarpaulins',
     image_path: '/assets/product-thumbnail/advertising-goods/tarpaulin.jpg',
     slug: 'advertising-goods'
   },
   {
     category: 'Appliances',
-    name: 'Television',
+    name: 'Televisions',
     image_path: '/assets/product-thumbnail/appliances/tv.jpg',
     slug: 'appliances'
   },
   {
     category: 'Appliances',
-    name: 'Oven',
+    name: 'Ovens',
     image_path: '/assets/product-thumbnail/appliances/oven.jpg',
     slug: 'appliances'
   },
   {
     category: 'Appliances',
-    name: 'Fan',
+    name: 'Fans',
     image_path: '/assets/product-thumbnail/appliances/fan.jpg',
     slug: 'appliances'
   },
   {
     category: 'Appliances',
-    name: 'Refrigerator',
+    name: 'Refrigerators',
     image_path: '/assets/product-thumbnail/appliances/refrigerator.jpg',
     slug: 'appliances'
   },
   {
     category: 'Awards & Citations',
-    name: 'Medal',
+    name: 'Medals',
     image_path: '/assets/product-thumbnail/awards-citations/medal.jpg',
     slug: 'awards-citations'
   },
   {
     category: 'Awards & Citations',
-    name: 'Plaque',
+    name: 'Plaques',
     image_path: '/assets/product-thumbnail/awards-citations/plaque.jpg',
     slug: 'awards-citations'
   },
   {
     category: 'Awards & Citations',
-    name: 'Frame',
+    name: 'Frames',
     image_path: '/assets/product-thumbnail/awards-citations/frame.jpg',
     slug: 'awards-citations'
   },
   {
     category: 'Awards & Citations',
-    name: 'Trophy',
+    name: 'Trophies',
     image_path: '/assets/product-thumbnail/awards-citations/trophy.jpg',
     slug: 'awards-citations'
   },
   {
     category: 'Collaterals',
-    name: 'Business Card',
+    name: 'Business Cards',
     image_path: '/assets/product-thumbnail/collaterals/business-card.jpg',
     slug: 'collaterals'
   },
   {
     category: 'Collaterals',
-    name: 'Brochure',
+    name: 'Brochures',
     image_path: '/assets/product-thumbnail/collaterals/brochure.jpg',
     slug: 'collaterals'
   },
   {
     category: 'Collaterals',
-    name: 'Bookmark',
+    name: 'Bookmarks',
     image_path: '/assets/product-thumbnail/collaterals/bookmark.jpg',
     slug: 'collaterals'
   },
   {
     category: 'Collaterals',
-    name: 'PVC ID Card',
+    name: 'PVC ID Cards',
     image_path: '/assets/product-thumbnail/collaterals/pvc-id.jpg',
     slug: 'collaterals'
   },
   {
     category: 'Corporate Giveaways',
-    name: 'Lanyard',
+    name: 'Lanyards',
     image_path: '/assets/product-thumbnail/corporate-giveaways/lanyard.jpg',
     slug: 'corporate-giveaways'
   },
   {
     category: 'Corporate Giveaways',
-    name: 'Mousepad',
+    name: 'Mousepads',
     image_path: '/assets/product-thumbnail/corporate-giveaways/mousepad.jpg',
     slug: 'corporate-giveaways'
   },
   {
     category: 'Corporate Giveaways',
-    name: 'Notebook',
+    name: 'Notebooks',
     image_path: '/assets/product-thumbnail/corporate-giveaways/notebook.jpg',
     slug: 'corporate-giveaways'
   },
   {
     category: 'Corporate Giveaways',
-    name: 'Umbrella',
+    name: 'Umbrellas',
     image_path: '/assets/product-thumbnail/corporate-giveaways/umbrella.jpg',
     slug: 'corporate-giveaways'
   },
   {
     category: 'Corporate Giveaways',
-    name: 'Pen',
+    name: 'Pens',
     image_path: '/assets/product-thumbnail/corporate-giveaways/pen.jpg',
     slug: 'corporate-giveaways'
   },
   {
     category: 'Corporate Giveaways',
-    name: 'Tote Bag',
+    name: 'Tote Bags',
     image_path: '/assets/product-thumbnail/corporate-giveaways/tote-bag.jpg',
     slug: 'corporate-giveaways'
   },
   {
     category: 'Corporate Giveaways',
-    name: 'Tumbler',
+    name: 'Tumblers',
     image_path: '/assets/product-thumbnail/corporate-giveaways/tumbler.jpg',
     slug: 'corporate-giveaways'
   },
   {
     category: 'Garments',
-    name: 'Jacket',
+    name: 'Jackets',
     image_path: '/assets/product-thumbnail/garments/jacket.jpg',
     slug: 'garments'
   },
   {
     category: 'Garments',
-    name: 'T-shirt',
+    name: 'T-shirts',
     image_path: '/assets/product-thumbnail/garments/tshirt.jpg',
     slug: 'garments'
   }
