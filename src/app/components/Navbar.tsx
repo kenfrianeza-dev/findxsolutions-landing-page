5cd764938789804ed246c721e2dc8c0f39ab2d78
Keynn2555 <75205651+Keynn2555@users.noreply.github.com>
2023-07-23 04:30:34 +0800

    window does not exist fixed

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,42 +2,34 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { TbMenu } from "react-icons/tb";
-// import { Link as SmoothLink } from "react-scroll";
 
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = () => {
 	const [color, setColor] = useState(false);
-	const [toggle, setToggle] = useState(false);
 
-	const changeColor = () => {
-		window.scrollY >= 90 ? setColor(true) : setColor(false);
-	};
-
-	window.addEventListener("scroll", changeColor);
-
-	// const toggleMobileMenu = () => {
-
-	// };
+	useEffect(() => {
+		const changeColor = () => {
+			window.scrollY >= 90 ? setColor(true) : setColor(false);
+		};
+		window.addEventListener("scroll", changeColor);
+	}, []);
 
 	const data = [
-		{ name: "Home", url: "/", offset: 0, id: "hero-section" },
+		{ name: "Home", url: "/" },
 		{
 			name: "Products",
 			url: "#products-section",
-			offset: 0,
-			id: "products-section",
 		},
-		{ name: "About Us", url: "/learn-more", offset: -125, id: "about-us" },
+		{ name: "About Us", url: "/learn-more" },
 		{
 			name: "Contact",
 			url: "#contact-section",
-			offset: -85,
-			id: "contact-section",
 		},
 	];
+
 	return (
 		<>
 			<nav
@@ -47,14 +39,7 @@ const Navbar: FC<NavbarProps> = () => {
 			>
 				{/* <nav className="fixed w-full text-white z-[999]"> */}
 				<div className="w-5/6 py-3 mx-auto flex justify-between">
-					<Link
-						href="/"
-						// to="hero-section"
-						// spy={true}
-						// smooth={true}
-						// duration={500}
-						className="flex items-center justify-center"
-					>
+					<Link href="/" className="flex items-center justify-center">
 						<Image src="/logo2.png" alt="Logo" width={50} height={50} />
 					</Link>
 					<div className="flex items-center justify-center">
@@ -68,16 +53,7 @@ const Navbar: FC<NavbarProps> = () => {
 						<ul className="lg:flex md:flex hidden gap-x-6 font-light">
 							{data.map((item) => (
 								<li key={item.name}>
-									<Link
-										href={item.url}
-										// to={item.id}
-										// spy={true}
-										// smooth={true}
-										// offset={item.offset}
-										// duration={500}
-									>
-										{item.name}
-									</Link>
+									<Link href={item.url}>{item.name}</Link>
 								</li>
 							))}
 						</ul>
--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { FC } from "react";
 import PolicyList from "./components/PolicyList";
 import PolicyHeader from "./components/PolicyHeader";
