00f2872686f231e842e2329422bc426e590496d2
Keynn2555 <75205651+Keynn2555@users.noreply.github.com>
2023-07-22 22:01:35 +0800

    minor changes to products

--- src/app/api/products/category/[slug]/route.ts
+++ src/app/api/products/category/[slug]/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from 'next/server';
-import { products } from '../../../../constants/products/data'
+import { items } from '../../../../constants/products/data'
 
 export async function GET(req: Request, { params }: { params: { slug: string } }) {
   const slug = params.slug
-  const data = products.filter((product: any) => product.slug === slug);
+  const data = items.filter((product: any) => product.slug === slug);
   return data.length > 0 ? 
     NextResponse.json({data}) : 
     NextResponse.json({message: `Product with category of ${slug} not found.`})
