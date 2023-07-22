34810fc1de14d5c3ff08be0f35ae369ce1c9c84e
Keynn2555 <75205651+Keynn2555@users.noreply.github.com>
2023-07-22 22:03:15 +0800

    minor changes

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
-import { products } from '../../constants/products/data'
+import { items } from '../../constants/products/data'
 
 export async function GET(req: Request) {
-  return NextResponse.json({data: products})
+  return NextResponse.json({data: items})
 }
