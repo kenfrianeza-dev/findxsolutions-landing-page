7772b708607fc11580ba0d33d762ef9ed8197b2b
Keynn2555 <75205651+Keynn2555@users.noreply.github.com>
2023-07-26 03:12:13 +0800

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -20,6 +21,7 @@ export default function RootLayout({
 				<Navbar />
 				{children}
 				<Footer />
+				<Analytics />
 			</body>
 		</html>
 	);
